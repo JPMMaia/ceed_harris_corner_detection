6d45022a8f731e576576bf7d5391f8b4fed9b89b
jpmmaia <jpmmaia@gmail.com>
2016-04-10 03:38:58 +0100

    Adding test project.

---
+++ TestProject/HarrisTester.cpp
@@ -0,0 +1,18 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TestProject
+{		
+	TEST_CLASS(HarrisTester)
+	{
+	public:
+		
+		TEST_METHOD(TestImage)
+		{
+			
+		}
+
+	};
+}
